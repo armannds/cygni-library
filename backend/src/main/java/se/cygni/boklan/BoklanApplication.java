fed12c8df3012e9fb784e26a240ed4534c3a4933
Rickard Englund <rickard.englund@gmail.com>
2018-05-05 09:46:26 +0200

    add backend template

---
+++ backend/src/main/java/se/cygni/boklan/BoklanApplication.java
@@ -0,0 +1,12 @@
+package se.cygni.boklan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoklanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoklanApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/se/cygni/boklan/BoklanApplicationTests.java
@@ -0,0 +1,16 @@
+package se.cygni.boklan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoklanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
